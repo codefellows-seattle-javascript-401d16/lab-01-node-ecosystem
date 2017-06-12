a53f5e30fe3f3b15c509f39da70969649a6121a4
derekcfarmer <derek.farmer@gmail.com>
2017-06-12 16:44:20 -0700

    tests working correctly

--- test/greet-test.js
+++ test/greet-test.js
@@ -1,5 +1,8 @@
 'use strict';
 
+const greet = require('../lib/greet.js');
+const expect = require('expect');
+
 describe ('greet', function (){
   it ('should return Hello User', function() {
     let greetTest = greet('User');
