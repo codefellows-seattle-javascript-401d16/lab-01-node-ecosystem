ca5599e9e64d956166f4b72599cf2ce2fda636f9
Sean Reigle <sereigle@gmail.com>
2017-06-12 16:31:40 -0700

    more changes

--- lab-sean/lib/arithmetic.js
+++ lab-sean/lib/arithmetic.js
@@ -2,7 +2,7 @@
 
 const arithmetic = module.exports = {};
 
-arithmetic.add = function add (a, b) {
+arithmetic.add = function(a, b) {
   if (typeof a == 'number' && typeof b == 'number') {
     return a + b;
   } else {
@@ -10,7 +10,7 @@ arithmetic.add = function add (a, b) {
   }
 };
 
-arithmetic.subtract = function subtract (a, b) {
+arithmetic.subtract = function(a, b) {
   if (typeof a == 'number' && typeof b == 'number') {
     return a - b;
   } else {
