ab1d2ecf03433e1ad4bfe8948c9095a3f8e53c81
Joshua Evans <skyfriends1@gmail.com>
2017-06-13 16:59:06 -0700

    added function for returning the tests for index that i wrote, and logged function below in index.js

--- lab-joshuaevans/index.js
+++ lab-joshuaevans/index.js
@@ -2,3 +2,9 @@
 
 const arithmetic = require('./lib/arithmetic.js');
 const greet = require('./lib/greet.js');
+
+const out = module.exports = () => {
+  return greet('Josh'), arithmetic.add(1,2), arithmetic.suv(3,2);
+};
+
+console.log(out());
