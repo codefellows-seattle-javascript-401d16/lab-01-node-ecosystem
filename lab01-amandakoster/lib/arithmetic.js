f87b11a7c475e785fad3246715d13005f089ee35
Amanda Koster <amandakoster@gmail.com>
2017-06-12 18:41:41 -0700

    cleaned out corpse code

--- lab01-amandakoster/lib/arithmetic.js
+++ lab01-amandakoster/lib/arithmetic.js
@@ -1,11 +1,5 @@
 'use strict';
 
-// Create a nodejs module named arithmetic
-// The arithmetic.js module should have add and sub methods
-// add should have to params that should expect a number, it should return the sum of the to numbers
-// sub should have to params that should expect a number, it should return the first argument minus the second argument
-// decide on an expected behavior for non number inputs
-
 const arithmetic = module.exports = {};
 
 arithmetic.add = (a, b) => {
