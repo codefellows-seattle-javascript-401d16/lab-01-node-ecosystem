708f061c2822013df30bfcf7255fdd853027139e
derekcfarmer <derek.farmer@gmail.com>
2017-06-13 09:59:16 -0700

    added comma to Hello, World

--- lab-derek/test/greet-test.js
+++ lab-derek/test/greet-test.js
@@ -4,8 +4,8 @@ const greet = require('../lib/greet.js');
 const expect = require('expect');
 
 describe ('greet', function (){
-  it ('should return Hello World', function() {
+  it ('should return Hello, World', function() {
     let greetTest = greet('World');
-    expect(greetTest).toEqual('Hello World');
+    expect(greetTest).toEqual('Hello, World');
   });
 });
