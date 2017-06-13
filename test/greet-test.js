5ac9c088c5f00e6a65fa054a577a3ead1d2a20d4
Oscar Cauich <ocauich@gravitypayments.com>
2017-06-12 17:40:11 -0700

    testing for invalid strings, returning null

--- test/greet-test.js
+++ test/greet-test.js
@@ -4,8 +4,19 @@ const expect = require('expect');
 const greet = require('../lib/greet.js');
 
 describe('testing greet function for valid strings', () =>{
-  it('should return Hello, World!', () => {
-    let result = greet('World!');
-    expect(result).toEqual('Hello, World!');
+
+  //testing for valid strings
+  describe('testing for valid string', () => {
+    it('should return Hello, World!', () => {
+      let result = greet('World!');
+      expect(result).toEqual('Hello, World!');
+    });
+  });
+  //testing for invalid string input
+  describe('testing for invalid input', () => {
+    it('should return null', () =>{
+      let result = greet(2);
+      expect(result).toEqual(null);
+    });
   });
 });
