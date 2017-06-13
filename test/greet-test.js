5f966b7a18a6c97e7bda80f819cf1f50a8ce2d0a
Saul Greene <spdysg@gmail.com>
2017-06-12 21:39:13 -0700

    passing all tests

--- test/greet-test.js
+++ test/greet-test.js
@@ -2,30 +2,14 @@
 const expect = require('expect');
 
 const greet = require('../lib/greet.js');
-const arithmetic = require('../lib/arithmetic.js');
 
-
-//   it('should return a valid string', function() {
-//     assert.equal(typeof greet.hello('saul'), 'string');
 
 
 describe('testing greeting', ()=>{
   it('should return hello saul', ()=>{
     expect(greet('world')).toEqual('hello world');
   });
-});
-
-
-
-describe('testing arithmetic', () => {
-  describe('testing add', ()=> {
-    it('should return 10', () =>{
-      expect(arithmetic.add(5, 5)).toEqual(10);
-    });
-  });
-  describe('testing subtract', ()=>{
-    it('should return 2', ()=>{
-      expect(arithmetic.subtract(8, 6)).toEqual(2);
-    });
+  it('should return a string', ()=>{
+    expect(greet('world')).toBeA('string', 'hello world');
   });
 });
