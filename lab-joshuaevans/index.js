f18bcd990c28437f93820a751b8c4941af0ea239
Joshua Evans <skyfriends1@gmail.com>
2017-06-13 17:17:14 -0700

    invoked functions

--- lab-joshuaevans/index.js
+++ lab-joshuaevans/index.js
@@ -3,8 +3,6 @@
 const arithmetic = require('./lib/arithmetic.js');
 const greet = require('./lib/greet.js');
 
-const out = module.exports = () => {
-  return greet('Josh'), arithmetic.add(1,2), arithmetic.suv(3,2);
-};
-
-console.log(out());
+greet('Joshua');
+arithmetic.add(3,2);
+arithmetic.sub(5,3);
