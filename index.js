92c214fa60569a9fd149a078aae1eb5c50dee736
Zachary Diehl <zachary.diehl@gmail.com>
2017-06-13 18:33:46 -0700

    uncommented index file

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-// 'use strict';
-//
-// const arithmetic = require('./lib/arithmetic.js');
-// const greet = require('./lib/greet.js');
-//
-// console.log('Index is working!');
+'use strict';
+
+const arithmetic = require('./lib/arithmetic.js');
+const greet = require('./lib/greet.js');
+
+
+console.log('Index is working!');
